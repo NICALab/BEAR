0fe60e0180cea9c96b3d73df5e0bb5e1d4e0363a
SteveJayH <jay0118@yonsei.ac.kr>
2021-07-07 14:15:39 +0900

    demomovie

--- scripts/run_BEAR.py
+++ scripts/run_BEAR.py
@@ -11,6 +11,7 @@ import skimage.io as skio
 import time as _time
 import torch
 import os
+import numpy as np
 import sys
 sys.path.append('.')
 from bear.BEAR import BEAR
@@ -47,6 +48,20 @@ def run(args):
             args.batch_size = 64
             args.device = 'cuda:0'
 
+    elif dataset == "demoMovie":
+        path = "./data/demoMovie.tif"
+        Y = torch.from_numpy(skio.imread(path).astype(float)).float().permute(1, 2, 0)
+        print(Y.size())
+        data_shape = list(Y.size())
+        Y_res = Y.reshape(np.prod(data_shape[0:2]), data_shape[2]).permute(1, 0)
+
+        if default:
+            print("Set to default setting!")
+            args.lr = 1e-3
+            args.epoch = 80
+            args.rank = 2
+            args.batch_size = 512
+            args.device = 'cuda:0'
 
     print(f"""
     START TIME : {_time.strftime("%Y-%m-%d %H:%M:%S", _time.localtime())}
